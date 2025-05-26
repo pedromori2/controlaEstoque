523add88ff08eeba9e47a76a116c55094513fba3
Pedro Mori <pedrohiroshigue@gmail.com>
2025-05-25 23:23:42 -0300

    Fix dos testes unitários/2

--- src/test/java/br/com/controlaestoque/controlaestoque/ProdutoControllerTests.java
+++ src/test/java/br/com/controlaestoque/controlaestoque/ProdutoControllerTests.java
@@ -17,7 +17,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class ProdutoControllerTests {
 
